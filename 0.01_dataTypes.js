3bc19d486a2167aed2c4e33f4fc33a789ae60cc3
Madyson Ward <MadysonWard94@gmail.com>
2017-08-16 11:39:17 -0400

    Added 0.01_datatypes js file

---
+++ 0.01_dataTypes.js
@@ -0,0 +1,67 @@
+//------------------------------------------------------------------------------------------------------------
+//Data Types
+//------------------------------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------------------------------
+//--- Strings:
+//------------------------------------------------------------------------------------------------------------
+"What a wonderful world!";
+'Hello everybody!';
+/* You can wrap single quotation marks within double quotation marks, and vice versa. For example: */
+"It's a wonderful day!";
+'Mom said "The sky is blue and the grass is green!"';
+/* If you're using single quotation marks, and need to use a single quote/apostrophe within that string, put a 
+backslash before the single quotation mark. */
+'Life\'s good.';
+console.log('Life\'s good');
+/* It can be a good idea to add spaces between the string indicator (double/single quotation marks) and 
+parentheses, to help with readability and simplify the process of finding mistakes. */
+console.log( "Life's good." );
+
+
+//------------------------------------------------------------------------------------------------------------
+//--------Strings: Functions: Methods & Properties
+//------------------------------------------------------------------------------------------------------------
+/* The below is a method function */
+console.log( 'Tiah said "Fifi was great on our walk today!"'.toUpperCase() );
+
+/* The below is a property function. */
+console.log( 'I like ice cream.'.length );
+
+
+/* Challenge: Create a string with your name, and use a function to make all characters lowercase.
+*/
+console.log( "Mady Ward".toLowerCase() );
+
+/* The main difference between a method and a property, is that a method stores a function, while a property
+stores a value. That's why a method function has the () at the end, and a property does not. */
+
+console.log( "mady"[0].toUpperCase());
+console.log("mady"[0].toUpperCase() + "mady"[1] + "mady"[2] + "mady"[3]);
+
+/* Strings are just characters. If you want to grab a specific character from a string, you can put brackers with
+the number/place of the character in the middle. The above, for example. */
+
+/* Chalenge: Uppercase the "m" in the Home and put the string back together using string concatenation. */
+//console.log( "Home"[2].toUpperCase() );
+console.log( "Home"[0] + "Home"[1] + "Home"[2].toUpperCase() + "Home"[3] );
+
+/**/
+//Booleans
+//------------------------------------------------------------------------------------------------------------
+//--- Numbers:
+//------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
