2ff4405cf153efa121473b6547b70368d6a9631b
Madyson Ward <MadysonWard94@gmail.com>
2017-08-16 12:12:59 -0400

    Modified datatypes.js file

--- 0.01_dataTypes.js
+++ 0.01_dataTypes.js
@@ -18,6 +18,11 @@ console.log('Life\'s good');
 parentheses, to help with readability and simplify the process of finding mistakes. */
 console.log( "Life's good." );
 
+console.log('\n');
+console.log('\n');
+console.log('\n');
+/*The above will come back as a new line. So each line is blank. */
+
 //------------------------------------------------------------------------------------------------------------
 //---Strings: Functions: Methods & Properties---
 //------------------------------------------------------------------------------------------------------------
@@ -37,31 +42,77 @@ stores a value. That's why a method function has the () at the end, and a proper
 
 console.log( "mady"[0].toUpperCase());
 console.log("mady"[0].toUpperCase() + "mady"[1] + "mady"[2] + "mady"[3]);
-
 /* Strings are just characters. If you want to grab a specific character from a string, you can put brackers with
 the number/place of the character in the middle. The above, for example. */
 
-/* Chalenge: Uppercase the "m" in the Home and put the string back together using string concatenation. */
+/* Challenge: Uppercase the "m" in the Home and put the string back together using string concatenation. */
 //console.log( "Home"[2].toUpperCase() );
 console.log( "Home"[0] + "Home"[1] + "Home"[2].toUpperCase() + "Home"[3] );
+
+/* The below function, .typeof() shows the data type... in this case, a string. */
+console.log(typeof("Amber"));
 //------------------------------------------------------------------------------------------------------------
 
-
-//------------------------------------------------------------------------------------------------------------
-//---Booleans:
-//------------------------------------------------------------------------------------------------------------
-
+console.log('\n');
+console.log('\n');
+console.log('\n');
+console.log('\n');
+console.log('\n');
 
 //------------------------------------------------------------------------------------------------------------
 //--- Numbers:
 //------------------------------------------------------------------------------------------------------------
+console.log(typeof(2));
+/* The above shows the data type as a number, to follow along with the last note in string functions. */
 
+console.log(2 + 5);
+console.log(2 - 5);
+console.log(2 / 5);
+console.log(2 * 5);
+console.log(5 % 2);
+/* The operators above are add, subtract, divide, multiply, and modulus (divide and give the remainder. ) */
+//------------------------------------------------------------------------------------------------------------
 
+console.log('\n');
+console.log('\n');
+console.log('\n');
+console.log('\n');
+console.log('\n');
 
+//------------------------------------------------------------------------------------------------------------
+//---Booleans:
+//------------------------------------------------------------------------------------------------------------
+console.log(true);
+console.log(false);
+/* Booleans are true or false. 
+Boolean Conditional Operators are the following:
+-  <   : less than
+-  >   : greater than
+-  <=  : less than or equal to
+-  >=  : greater than or equal to
+-  ==  : equal to
+-  === : strictly equal to
+-  !   : not
+-  !=  : not equal to
+ */
 
+console.log(4 == 5);
+/* The above would come back as false. */
 
+console.log("Test".length > "ego".length);
+/* The above would ring back as true, because the amount of characters in "Test" are greater than the amount in
+"ego" */
+console.log(1 == "1");
+console.log(1 === "1");
+/* For the above, the first comparison would come back as true, and the second as false. This is because == is
+equal to, and === is EXACTLY equal to...meaning it compares everything, including data type. */
 
-
+console.log('\n');
+console.log('\n');
+console.log('\n');
+console.log('\n');
+console.log('\n');
+//------------------------------------------------------------------------------------------------------------
 
 
 
