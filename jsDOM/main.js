ac37b7921d6bcd7f2e7097001b9c6645e639c2fc
Madyson Ward <MadysonWard94@gmail.com>
2017-08-22 16:05:41 -0400

    Added to index html file, and main css and js files. Completed 3 challenges in main js file

--- jsDOM/main.js
+++ jsDOM/main.js
@@ -1,10 +1,11 @@
 let input = document.getElementById("input");
 let output = document.getElementById("output");
+let ulist = document.getElementById("friendsList");
+let newName = "";
 //console.log(input.value);
-
+	
+input.focus(); //.focus allows the page to open with the form ready to be typed in! 
 function capitalize() {
-	let newName = "";
-
 	for ( let l in input.value ) {
 		if ( l == 0 ) {
 			newName = input.value[l].toUpperCase();
@@ -14,4 +15,41 @@ function capitalize() {
 	}
 
 	output.innerHTML = "Capitalized Name: " + newName;
+	input.value = "";
+	addItem();
 }
+
+
+/* BRONZE CHALLENGE: Find out how to have a conditional target the 'enter key', so that once it's 
+pressed, it can submit the form. Note: the below is called an event! */
+
+document.onkeypress = function(e) {
+	console.log(e)
+	if ( e.keyCode === 13 ) {
+		capitalize()
+	}
+}
+
+
+/* SILVER CHALLENGE: After a name is capitalized, make the form clear. */
+// See line 17
+
+
+/* GOLD CHALLENGE: Create an unordered list that prints out each name that a user wants to 
+capitalize without bullet points.*/
+
+function addItem() {
+	let li = document.createElement("li");
+	let text = li.innerHTML = newName;
+
+	ulist.appendChild(li);
+}
+//Also see line 18
+
+/*  
+To add a new HTML element through JavaScript, use .createElement()
+
+let test = document.createElement("li");
+console.log(test);
+
+*/
