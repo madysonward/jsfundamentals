cb0db75a8ce3a769d07fa325ed4b521ee0eb26e5
Madyson Ward <MadysonWard94@gmail.com>
2017-08-24 15:44:51 -0400

    Updated jQuery library and friends list files

--- jQueryFriendsList/public/main.js
+++ jQueryFriendsList/public/main.js
@@ -1,13 +1,35 @@
 $(document).ready(function(){
 	let names;
+
 	$.ajax({
 		type: "GET",
-		url: "http://rest.learncode.academy/api/learncode/friends",
+		url: "http://rest.learncode.academy/api/learncode/javascriptfall",
 	}).done(function(data){
 		names = data;
 		console.log(data);
 		console.log(names)
 	})
+
 	console.log("Hello!");
 	console.log(names)
+
+	let person = {
+		name: "",
+		email: "",
+	}
+
+	$.ajax({
+		type: "POST",
+		url: "http://rest.learncode.academy/api/learncode/javascriptfall",
+		data: person
+	}).done(function(info){
+		console.log(info);
+	})
+	$(submitBTN).click(){
+		person.name 
+		person.email
+	}
+
 })
+
+/* You can grab something out of an input box using .val */
