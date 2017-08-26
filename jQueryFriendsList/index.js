5913e4989f6505c8bb8fa07adf1a6655d5036899
Madyson Ward <MadysonWard94@gmail.com>
2017-08-26 16:11:13 -0400

    Completed jQueryFriendsList challenge

--- jQueryFriendsList/index.js
+++ jQueryFriendsList/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const app = express();
-const http = require("http").Server(app);
+let express = require("express");
+let app = express();
+let http = require("http").Server(app);
 
 app.use(express.static(__dirname + "/public"));
 
--- jQueryFriendsList/public/main.js
+++ jQueryFriendsList/public/main.js
@@ -1,47 +1,18 @@
-$(document).ready(function(){
-
-	$.ajax({
-		type: "GET",
-		url: "http://rest.learncode.academy/api/learncode/javascriptfall",
-	}).done(function(data){
-		console.log(data);
-	})
-	
-
-	
-
-	$(submitBTN).click(function(){
-		let person = {
-			name: inputName.value,
-			email: inputEmail.value,
-		}
-
-
-		console.log(person)
-		$.ajax({
-			type: "POST",
-			url: "http://rest.learncode.academy/api/learncode/javascriptfall",
-			data: person
-		}).done(function(info){
-			console.log(info);
-		})
-	})
-
-})
-
 var $friends = $("#friends");
 var $name = $("#name");
 var $age = $("#age");
 var friendTemplate = "" +
-	"li" + 
+	"<li>" + 
 	"<p><strong>Name:</strong> {{name}}</p>" +		
 	"<p><strong>Age:</strong>  {{age}}</p>" +
 	"<button id='{{id}}'' class='remove'>X</button>" +
 	"</li>";
 
+
 function addFriend(friend){
-	$(friends).append(Mustache.render(friendTemplate, friend));
-}
+	$friends.append(Mustache.render(friendTemplate, friend));
+};
+
 
 
 $(document).ready(function(){
@@ -58,11 +29,13 @@ $(document).ready(function(){
 		}
 	});
 
+	
 	$("#addFriend").on("click", function(){
-		let friend = {
-			name = $name.val(),
-			age = $age.val()
-		};
+
+		var friend = {
+			name: $name.val(),
+			age: $age.val()
+		}
 		$.ajax({
 			type: "POST",
 			url: "http://rest.learncode.academy/api/learncode/friends",
@@ -73,37 +46,24 @@ $(document).ready(function(){
 			error: function(){
 				alert("Error saving order!");
 			}
-		});
-	});
+		})
 
+	})
 
+	$friends.delegate(".remove", "click", function(){
+		var $li = $(this).closest("li");
+
+		$.ajax({
+			type: "DELETE",
+			url: "http://rest.learncode.academy/api/learncode/friends/" + $(this).attr("id"),
+			success: function(){
+				$li.fadeOut(300, function(){
+					$(this).remove();
+				})
+			}
+		})
+	})
 
 });
-
-
-//Below is (maybe) scratch. need to review
-
-
-
-	// $("#addFriend").on("click", function(){
-	// 	var friend = {
-	// 		name = $name.val(),
-	// 		age = $age.val()
-	// 	}
-
-	// });
-
-	// $.ajax({
-	// 	type: "POST",
-	// 	url: "http://rest.learncode.academy/api/learncode/friends",
-	// 	data: friend,
-	// 		success: function(newFriend){
-	// 		addFriend(newFriend);
-	// 	},
-	// 		error: function(){
-	// 		alert("Error saving order.")
-	// 	}
-	// });
-
 
 /* You can grab something out of an input box using .val */
