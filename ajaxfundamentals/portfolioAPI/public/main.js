f2f639d0d658f45368bc3589145071445aaa6e42
Madyson Ward <MadysonWard94@gmail.com>
2017-08-28 14:49:42 -0400

    Added football img to the public folder of the portfolioAPI folder. Finished up AJAX Challenge within portfolioAPI folder files.

--- ajaxfundamentals/portfolioAPI/public/main.js
+++ ajaxfundamentals/portfolioAPI/public/main.js
@@ -32,35 +32,40 @@ $(document).ready(function(){
 			})
 		})
 	}
-	// if(!localStorage.getItem("crimes21")){
-	// 	let crimeObj2 = {
-	// 		crimes2: []
-	// 	}
-	// 	for(let x = 1; x < 6; x++){
-	// 		$.ajax({
-	// 			type: "GET",
-	// 			url: "http://nflarrest.com/api/v1/crime/?start_pos=63&limit=5" - x + "http://nflarrest.com/api/v1/crime/?start_pos=97&limit=5",
-	// 		}).done(function(data){
-	// 			crimeObj2.crimes.push(data.Category)
-	// 			localStorage.setItem("crimes2", JSON.stringify(crimeObj2))
-	// 		})
-	// 	}
-	// }
+	if(!localStorage.getItem("crimes21")){
+		let crimeObj2 = {
+			crimes2: []
+		}
+		
+			$.ajax({
+				type: "GET",
+				url: "http://nflarrest.com/api/v1/crime/?start_pos=97&limit=5",
+			}).done(function(data){
+				console.log(data);
+				data.forEach(function(n){
+					$("#crimes2").append("<tr><td>" + n + "</td></tr>")
+				crimeObj2.crimes2.push(n.Category)
+				})	
+				localStorage.setItem("crimes2", JSON.stringify(crimeObj2))
+			})
+		
+	}
 
 	let click = false;
 	$(crimes2).on("click", function(){
 		click = !click
-		$(crimes2).val("Crimes")
+		$(crimes2).val("Next")
 		$("#crimes").html("")
 		if(click){
-			$(change).html("Crimes");
+			$(change).html("Top Crimes");
 			let crimeObj2 = JSON.parse(localStorage.getItem("crimes2"));
 			crimeObj2.crimes2.forEach(function(e){
-				$("crimes").append("<tr><td>" + e + "</td></tr>");
+				console.log(e)
+				$(crimes).append("<tr><td>" + e + "</td></tr>");
 			})
 		} else {
 			$(crimes2).val("Next")
-			$(change).html("Next")
+			$(change).html("Top Crimes")
 			let crimeObj = JSON.parse(localStorage.getItem("crimes"));
 			crimeObj.crimes.forEach(function(h){
 				$("#crimes").append("<tr><td>" + h + "</td></tr>");
